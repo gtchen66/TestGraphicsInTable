eb680ad5243f6990c74b39f5eb7728b8f11df15b
gtchen66 <gtchen66@yahoo.com>
2014-02-15 22:56:54 -0800

    Initial Commit

---
+++ TestGraphicsInTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestGraphicsInTable
+//
+//  Created by George Chen on 2/15/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
